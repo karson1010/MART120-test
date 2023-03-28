c2f3bb9380c945c646a94961a3ba92da16112425
karson1010 <123289056+karson1010@users.noreply.github.com>
2023-03-27 22:18:44 -0600

    upload file and image

---
+++ Homework 9/sketch.js
@@ -0,0 +1,45 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+ellipse(175,100,300,200);
+     point(95, 30)
+  
+   line(200, 50, 65, 45)
+  line(300, 50, 65, 45)
+  line(215, 270, 215, 270)
+  square(90,40,50)
+   square(200,40,50)
+   point(130, 75)
+  point(230, 75)
+  rect(145,130,40,30)
+  rect(80,20,200,10)
+  
+  ellipse(105,70,10,10)
+  ellipse(210,70,10,10)
+  rect(110,190,130,90)
+  rect(110,270,40,70)
+  rect(200,270,40,70)
+  rect(90,310,30,30)
+  rect(180,310,30,30)
+  
+   triangle(80, 75, 58, 20, 36, 45)
+    triangle(10, 75, 38, 20, 36, 45)
+    ellipse(35,40,10,10)
+  
+   rect(60,190,40,70)
+  circle(70,270,40)
+   rect(200,190,110,20)
+   circle(170,200,40)
+  
+
+    text('Karson tries on', 300, 30)
+    text('a bow', 300, 45)
+  
+     text('Karson McGrath', 110, 370)
+  
+
+
+}
